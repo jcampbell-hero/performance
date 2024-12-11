cfda74b366438017686149365d4d7b3a09781593
jcampbell-hero <55844524+jcampbell-hero@users.noreply.github.com>
2024-12-11 15:29:43 -0500

    Add files via upload

---
+++ js/script-optimized.js
@@ -0,0 +1,18 @@
+var button = document.getElementById("btnSubmit");
+var fname;
+var lname;
+
+button.addEventListener("click",buttonClick);
+function buttonClick(){
+    fname = document.getElementById("fname").value;
+    lname = document.getElementById("lname").value;
+    console.log(fname+" "+lname);
+
+    if(fname.length < 1 || lname.length < 1){
+        alert("All fields are required.");
+        return;
+    }
+
+    var submissionElement=document.getElementById("nameForm");
+    submissionElement.innerHTML="Submission received! Thank you, "+fname+" "+lname+".";
+}
---
+++ js/script.js
@@ -0,0 +1,35 @@
+var button = document.getElementById("btnSubmit");
+var fname;
+var lname;
+
+setTimeout(function(){
+    console.log("Script completed");
+
+    var newImage=document.createElement("img");
+    newImage.setAttribute('src','images/site-logo-75.jpg');
+    newImage.setAttribute('alt','');
+    var headerElement=document.getElementById("header");
+    headerElement.prepend(newImage);
+
+    var navElement=document.getElementById("nav");
+    var newAlert=document.createElement("div");
+    newAlert.setAttribute('id','alert');
+    newAlert.innerText="Alert message: This site may be poorly optimized.";
+    navElement.append(newAlert);
+}, 2000);
+
+button.addEventListener("click",buttonClick);
+
+function buttonClick(){
+    fname = document.getElementById("fname").value;
+    lname = document.getElementById("lname").value;
+    console.log(fname+" "+lname);
+
+    if(fname.length < 1 || lname.length < 1){
+        alert("All fields are required.");
+        return;
+    }
+
+    var submissionElement=document.getElementById("nameForm");
+    submissionElement.innerHTML="Submission received! Thank you, "+fname+" "+lname+".";
+}
